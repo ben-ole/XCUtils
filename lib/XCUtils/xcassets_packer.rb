107a07f3a8304e436315cf3294b7d3f2f7046d11
Benjamin Müller <elchbenny@googlemail.com>
2015-01-13 16:58:30 +0100

    added scale options as cli options

--- lib/XCUtils/xcassets_packer.rb
+++ lib/XCUtils/xcassets_packer.rb
@@ -10,6 +10,12 @@ module XCUtils
     argument :source
     argument :target
 
+    class_options :scale_iphone_1x => 0.25
+    class_options :scale_iphone_2x => 0.5
+    class_options :scale_iphone_3x => 1.0
+    class_options :scale_ipad_1x   => 0.5
+    class_options :scale_ipad_2x   => 1.0
+
     def create_output_directory
       say_status "create_output_directory", nil
       name = File.basename target
@@ -36,29 +42,32 @@ module XCUtils
 
         # create @3x version
         say_status "create @3x version", "#{fn}@3x", :yellow
-        img.write(File.join(imageset_folder,"#{fn}@3x.png"))
+        img_3x = XCUtilsImageHandling.scale_image(img,options[:scale_iphone_3x])
+        img_3x.write(File.join(imageset_folder,"#{fn}@3x.png"))
         content_iphone_3x = {"idiom" => "iphone", "scale" => "3x", "filename" => "#{fn}@3x.png"}
 
         # create ipad retina version
         say_status "create ipad retina version", "#{fn}@2x~ipad", :yellow
-        img.write(File.join(imageset_folder,"#{fn}@2x~ipad.png"))
+        img_ipad_2x = XCUtilsImageHandling.scale_image(img,options[:scale_ipad_2x])
+        img_ipad_2x.write(File.join(imageset_folder,"#{fn}@2x~ipad.png"))
         content_ipad_2x = {"idiom" => "ipad", "scale" => "2x", "filename" => "#{fn}@2x~ipad.png"}
 
         # create ipad non retina version
         say_status "create ipad non retina version", "#{fn}~ipad", :yellow
-        img = XCUtilsImageHandling.scale_image(img,0.5)
-        img.write(File.join(imageset_folder,"#{fn}~ipad.png"))
+        img_ipad_1x = XCUtilsImageHandling.scale_image(img,options[:scale_ipad_1x])
+        img_ipad_1x.write(File.join(imageset_folder,"#{fn}~ipad.png"))
         content_ipad = {"idiom" => "ipad", "scale" => "1x", "filename" => "#{fn}~ipad.png"}
 
         # create iphone retina version - identical to ipad non retina version
         say_status "create iphone retina version - identical to ipad non retina version", "#{fn}@2x", :yellow
-        img.write(File.join(imageset_folder,"#{fn}@2x.png"))
+        img_iphone_2x = XCUtilsImageHandling.scale_image(img,options[:scale_iphone_2x])
+        img_iphone_2x.write(File.join(imageset_folder,"#{fn}@2x.png"))
         content_iphone_2x = {"idiom" => "iphone", "scale" => "2x", "filename" => "#{fn}@2x.png"}
 
         # create iphone non retina version
         say_status "create iphone non retina version", "#{fn}", :yellow
-        img = XCUtilsImageHandling.scale_image(img,0.5)
-        img.write(File.join(imageset_folder,"#{fn}.png"))
+        img_iphone_1x = XCUtilsImageHandling.scale_image(img,options[:scale_iphone_1x])
+        img_iphone_1x.write(File.join(imageset_folder,"#{fn}.png"))
         content_iphone = {"idiom" => "iphone", "scale" => "1x", "filename" => "#{fn}.png"}
 
         # create json
@@ -70,5 +79,19 @@ module XCUtils
 
       end
     end
+
+    # private
+    #
+    #   def class_options
+    #     original_options = super
+    #     file_path = File.join(source, '.xc-config')
+    #     return original_options unless File.exists?(file_path)
+    #
+    #     say_status "found configuration file", nil
+    #     defaults = load file_path
+    #
+    #     Thor::CoreExt::HashWithIndifferentAccess.new( defaults.merge(original_options) )
+    #   end
+
   end
 end
